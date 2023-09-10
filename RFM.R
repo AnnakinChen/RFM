c2c870d9d1c07363c7e4d6a1bee09fbff8fcedba
AnnakinChen <128199338+AnnakinChen@users.noreply.github.com>
2023-09-10 14:31:04 +0800

    codes and data

---
+++ RFM.R
@@ -0,0 +1,43 @@
+library(shiny)
+library(shinydashboard)
+library(shinyauthr)
+library(dplyr)
+library(shinythemes)
+library(readxl)
+library(ggplot2)
+library(DT)
+
+
+user_base <- dplyr::tibble(
+  user = c("Skywalker"),
+  password = c("May the force be with you"),
+  permissions = c("admin"),
+  name = c("User One")
+)
+options(shiny.maxRequestSize = 50*1024^2)
+source('ui.R')
+source('server.R')
+
+shinyApp(ui = ui, server = server)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
